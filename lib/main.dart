3abd5e7ae2147151c0efce761143506a21cce1f3
shihabksaleem <shihab.ks56@gmail.com>
2023-01-12 15:14:14 +0530

    lottie animation updated

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ExpansionTileCardSample(),
+      initialRoute: ExpansionTileCardSample.id,
+      routes: {
+        ExpansionTileCardSample.id: (context) => ExpansionTileCardSample()
+      },
     );
   }
 }
